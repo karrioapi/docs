080edeb9401808fd76ffa03403879ba5717e3f2f
DanH91 <danielk.developer@gmail.com>
2021-11-03 10:43:52 -0400

    remove bulma pages

--- src/components/styles.tsx
+++
@@ -1,5 +0,0 @@
-import bulma from './index.module.scss';
-
-export default function clss(classes: string): string {
-  return classes.split(' ').map(c => bulma[c]).join(' ');
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,38 @@
 import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import Navbar from '../components/navbar';
-import HeroBody from '../components/hero-body';
-import HeroFooter from '../components/hero-footer';
-import IntroSection from '../components/intro-section';
-import Footer from '../components/footer';
-import clss from '../components/styles';
+import styles from './index.module.scss';
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Docusaurus Tutorial - 5min ⏱️
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
-
   return (
-    <>
-      <Navbar />
-
-      <section className={clss("hero is-primary is-fullheight px-0 pb-0")}>
-        <HeroBody />
-
-        <HeroFooter />
-      </section>
-
-      <IntroSection />
-
-      <Footer />
-    </>
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+      </main>
+    </Layout>
   );
 }
